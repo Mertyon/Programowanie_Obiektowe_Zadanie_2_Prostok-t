6d358ce624dcf59b532871e9d64189652937455d
Mertyon <81264329+Mertyon@users.noreply.github.com>
2022-06-22 20:24:53 +0200

    podstawy
    zaczęte pobieżnie

--- PrzemysławTrubiłoZad2/PrzemysławTrubiłoZad2/Program.cs
+++ PrzemysławTrubiłoZad2/PrzemysławTrubiłoZad2/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
-
+﻿ using System;
+using System.Collections.Generic;
 namespace PrzemysławTrubiłoZad2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+    }
+
+    class Prostokąt
+    {
+        double a, b;
+        
+        public Prostokąt()
+        {
+            double bokA, bokB;
+            
+        }
+
+        public static void Słownik()
+        {
+            Dictionary<char, double> wysokośćArkusza = new Dictionary<char, double>
+            {
+                ['A'] = 1189,
+                ['B'] = 1414,
+                ['C'] = 1297
+            };
+        }
+
+
     }
 }
